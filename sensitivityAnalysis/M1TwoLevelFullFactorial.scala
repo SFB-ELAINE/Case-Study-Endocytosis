829a194ae1311f06ce39461fd6c5db7c62a26ec5
pw109 <pia.wilsdorf@uni-rostock.de>
2020-08-17 14:41:37 +0200

    added the generated simulation experiments and SESSL quickstart

---
+++ sensitivityAnalysis/M1TwoLevelFullFactorial.scala
@@ -0,0 +1,45 @@
+import sessl._
+import sessl.mlrules._
+import sessl.analysis.sensitivity._
+
+
+analyze((params, objective) =>
+  execute(
+
+    new Experiment with Observation with ExpressionObservation {
+
+      model = "../models/M1_General.mlrj"
+      set("nLrp6" <~ 4000)
+      set("nWnt" <~ 2000)
+      for ((name, value) <- params) {
+        set(name <~ value)
+      }
+
+      simulator = SimpleSimulator()
+      replications = 300
+      stopTime = 61
+
+      observeAt(61)
+      val R = observe(count("Membrane/Lrp6 (uB) "))
+      val LR = observe(count("Membrane/Lrp6 (B) "))
+      val allR = observe(Expr(N => N(R) + N(LR)))
+
+      withExperimentResult(results =>
+        objective <~ results.mean(allR)
+      )
+
+    }
+  )
+) using new TwoLevelFullFactorialSetup {
+
+    baseCase(
+      "ke" <~ 0.2
+    )
+    sensitivityCase(
+      "ke" <~ 0.4
+    )
+
+    withAnalysisResult(result => {
+      result.printCompleteReport()
+    })
+}
---
+++ statisticalModelChecking/M3StatisticalModelChecking.scala
@@ -0,0 +1,47 @@
+import sessl._
+import sessl.mlrules._
+import sessl.verification._
+import sessl.verification.mitl._
+
+
+object Reference {
+  val ref: Trajectory[Double] = List(
+    (0, 1),
+    (10, 0.6408),
+    (20, 0.5563),
+    (30, 0.4554),
+    (60, 0.3944),
+    (61, 0.3944)
+  )
+}
+
+execute(
+
+  new Experiment with Observation with ExpressionObservation 
+                            with StatisticalModelChecking {
+
+    model = "../models/M3_Wnt.mlrj"
+    set("nR" <~ 4000)
+    set("nL" <~ 2000)
+    set("ke_raft" <~ 0.1)
+    set("ke_nonraft" <~ 0.1)
+
+    simulator = SimpleSimulator()
+    stopTime = 61
+
+    observeAt(0, 10, 20, 30, 60, 61)
+    val Lrp6 = observe(count("Cell/Membrane/Lrp6(_, _)"))
+    val Lrp6Axin = observe(count("Cell/Membrane/Lrp6Axin(_)"))
+    val Raft_Lrp6 = observe(count("Cell/Membrane/LR/Lrp6(_, _)"))
+    val Raft_Lrp6Axin = observe(count("Cell/Membrane/LR/Lrp6Axin(_)"))
+    val allR = observe(Expr(N => N(Lrp6) + N(Lrp6Axin) + N(Raft_Lrp6) + N(Raft_Lrp6Axin)))
+
+    test = SequentialProbabilityRatioTest(p = 0.8, alpha = 0.05, beta = 0.05, delta = 0.05)
+    prop = MITL( G(0, 60)( 
+            (OutVar(allR)/Constant(4000) - Traj(Reference.ref)).abs < Constant(0.2)) )
+
+    withCheckResult { result =>
+      println(result.satisfied)
+    }
+  }
+)
